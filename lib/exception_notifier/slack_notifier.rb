0525fd72afffc9c43fd79c8c4df0551f69c77f70
hoangdd <hoang@kizuki.co.jp>
2017-05-25 22:25:51 +0900

    Slack multi channels

--- lib/exception_notifier/slack_notifier.rb
+++ lib/exception_notifier/slack_notifier.rb
@@ -13,6 +13,12 @@ module ExceptionNotifier
         webhook_url = options.fetch(:webhook_url)
         @message_opts = options.fetch(:additional_parameters, {})
         @color = @message_opts.delete(:color) { 'danger' }
+
+        # Channel control
+        @channels = options.fetch(:channels)
+        @default_channel = @channels["default"] || "#general"
+        options[:channel] = @default_channel)
+
         @notifier = Slack::Notifier.new webhook_url, options
       rescue
         @notifier = nil
@@ -57,6 +63,7 @@ module ExceptionNotifier
       attchs = [color: @color, text: text, fields: fields, mrkdwn_in: %w(text fields)]
 
       if valid?
+        @message_opts[:channel] = @channels[exception.class.name]|| @default_channel
         send_notice(exception, options, clean_message, @message_opts.merge(attachments: attchs)) do |msg, message_opts|
           @notifier.ping '', message_opts
         end
