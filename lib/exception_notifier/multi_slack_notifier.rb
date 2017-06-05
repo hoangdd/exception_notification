ce4f8ebd29906c3bd1194d840d730e97851d7482
hoangdd <hoang@kizuki.co.jp>
2017-06-05 12:34:29 +0900

    Change exception message format

--- lib/exception_notifier/multi_slack_notifier.rb
+++ lib/exception_notifier/multi_slack_notifier.rb
@@ -1,91 +1,70 @@
 module ExceptionNotifier
-  class MultiSlackNotifier < BaseNotifier
+  class MultiSlackNotifier < SlackNotifier
     include ExceptionNotifier::BacktraceCleaner
-
-    attr_accessor :notifier
-
+ 
     def initialize(options)
       super
       begin
-        @ignore_data_if = options[:ignore_data_if]
-        @backtrace_lines = options.fetch(:backtrace_lines, 10)
-
-        webhook_url = options.fetch(:webhook_url)
-        @message_opts = options.fetch(:additional_parameters, {})
-        @color = @message_opts.delete(:color) { 'danger' }
-
         # Channel control
         @channels = options.fetch(:channels)
         @default_channel = @channels["default"]
-
-        @notifier = Slack::Notifier.new webhook_url, options
       rescue
         @notifier = nil
       end
     end
-
+ 
     def call(exception, options={})
       errors_count = options[:accumulated_errors_count].to_i
-      measure_word = errors_count > 1 ? errors_count : (exception.class.to_s =~ /^[aeiou]/i ? 'An' : 'A')
-      exception_name = "*#{measure_word}* `#{exception.class.to_s}`"
-
+      exception_name = "*Exception*: `#{exception.class.to_s}` \n"
+      fields = []
+ 
+      text = exception_name
+      message_text = ">>> " + exception.message.gsub("`", "'") + "\n\n"
+ 
       if options[:env].nil?
         data = options[:data] || {}
-        text = "#{exception_name} *occured in background*\n"
+        env_text = "*Process:* occured in background\n"
       else
         env = options[:env]
         data = (env['exception_notifier.exception_data'] || {}).merge(options[:data] || {})
-
+ 
         kontroller = env['action_controller.instance']
-        request = "#{env['REQUEST_METHOD']} <#{env['REQUEST_URI']}>"
-        text = "#{exception_name} *occurred while* `#{request}`"
-        text += " *was processed by* `#{kontroller.controller_name}##{kontroller.action_name}`" if kontroller
-        text += "\n"
+        request = "`#{env['REQUEST_METHOD']}` <#{env['REQUEST_URI']}>"
+        env_text = "*Request*: #{request}\n"
+        env_text += "*Process*: `#{kontroller.class.name}##{kontroller.action_name}`" if kontroller
+        env_text += "\n"
       end
-
-      clean_message = exception.message.gsub("`", "'")
-      fields = [ { title: 'Exception', value: clean_message } ]
-
+ 
+      fields.push({ title: 'Message', value: message_text })
+      fields.push({ title: '', value: env_text })
       fields.push({ title: 'Hostname', value: Socket.gethostname })
-
+ 
       if exception.backtrace
-        formatted_backtrace = "```#{exception.backtrace.first(@backtrace_lines).join("\n")}```"
-        fields.push({ title: 'Backtrace', value: formatted_backtrace })
+        backtrace = exception.backtrace.first(@backtrace_lines)
+        formatted_backtrace = "```#{backtrace.join("\n")}```"
+        gem_paths = ENV["GEM_PATH"].split(':')
+        gem_paths.each do |path|
+          formatted_backtrace.gsub! path.to_s, "GEM_PATH"
+        end
+        formatted_backtrace.gsub! Rails.root.to_s, ""
+ 
+        fields.push({ title: 'Backtrace', value: formatted_backtrace, short: true })
       end
-
+ 
       unless data.empty?
         deep_reject(data, @ignore_data_if) if @ignore_data_if.is_a?(Proc)
         data_string = data.map{|k,v| "#{k}: #{v}"}.join("\n")
         fields.push({ title: 'Data', value: "```#{data_string}```" })
       end
-
+ 
       attchs = [color: @color, text: text, fields: fields, mrkdwn_in: %w(text fields)]
-
+ 
       if valid?
         @message_opts[:channel] = @channels[exception.class.name] || @default_channel
-        send_notice(exception, options, clean_message, @message_opts.merge(attachments: attchs)) do |msg, message_opts|
+        send_notice(exception, options, text, @message_opts.merge(attachments: attchs)) do |msg, message_opts|
           @notifier.ping '', message_opts
         end
       end
     end
-
-    protected
-
-    def valid?
-      !@notifier.nil?
-    end
-
-    def deep_reject(hash, block)
-      hash.each do |k, v|
-        if v.is_a?(Hash)
-          deep_reject(v, block)
-        end
-
-        if block.call(k, v)
-          hash.delete(k)
-        end
-      end
-    end
-
   end
-end
+ end
